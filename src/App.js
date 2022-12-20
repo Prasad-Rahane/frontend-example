61cf39235228b02cb2acb96577ca790e1a26ba1b
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-20 12:42:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ import UserProfile from './components/Screens/UserProfile'
 import SubscribedUserPosts from './components/Screens/SubscribesUserPosts'
 //import Reset from './components/screens/Reset'
 //import NewPassword from './components/screens/Newpassword'
+const url='https://insta12.adaptable.app/'
+
 export const UserContext = createContext()
+
 
 const Routing = ()=>{
   const history = useHistory()
